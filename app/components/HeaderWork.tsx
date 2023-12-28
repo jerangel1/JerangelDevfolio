a5bca51eff991ff0193524b6753290141a97473c
Jesus Rangel <jerangel1691@gmail.com>
2023-12-28 01:07:06 -0400

    section work

---
+++ app/components/HeaderWork.tsx
@@ -0,0 +1,19 @@
+
+export const HeaderWork = () => {
+    return (
+<div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <div className="flex items-center justify-center">
+            <iframe src="https://lottie.host/embed/3945cf96-0368-4bf5-b18e-fde844ca8201/kwqjXGwoDk.json"></iframe>
+          </div>
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+            From Travel Guru ✈ to Tech Ninja
+          </h2>
+          <p className="mt-4 text-xl text-zinc-400 mb-5">
+            In this section, I showcase my professional journey, spanning three distinct landscapes: tourism, logistics, and web development.
+          </p>
+        </div>
+      </div>
+    )
+}
+export default HeaderWork;
