8a0c0bb4d76b93f60e3a7394f25bd392d9c977b0
Jesus Rangel <jerangel1691@gmail.com>
2024-01-06 23:36:26 -0400

    fix mobile theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,11 +13,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-black ${inter.className}`}>{children}</body>
     </html>
-  )
+  );
 }
