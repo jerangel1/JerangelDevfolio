c95c8ecbd7306803b12f7da3b14521119f56cad2
Jesus Rangel <jerangel1691@gmail.com>
2024-01-08 00:26:42 -0400

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,75 @@
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+module.exports = {
+	content: [
+	  './pages/**/*.{js,ts,jsx,tsx,mdx}',
+	  './components/**/*.{js,ts,jsx,tsx,mdx}',
+	  './app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+	  extend: {
+		backgroundImage: {
+		  'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+		  'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+		},
+		animation: {
+		  'fade-in': 'fade-in 3s ease-in-out forwards',
+		  'title': 'title 3s ease-out forwards',
+		  'fade-left': 'fade-left 3s ease-in-out forwards',
+		  'fade-right': 'fade-right 3s ease-in-out forwards',
+		  'logo': 'logo 3s ease-in-out forwards',
+		},
+		keyframes: {
+		  'fade-in': {
+			'0%': {
+			  opacity: '0%',
 			},
-			animation: {
-				"fade-in": "fade-in 3s ease-in-out forwards",
-				title: "title 3s ease-out forwards",
-				"fade-left": "fade-left 3s ease-in-out forwards",
-				"fade-right": "fade-right 3s ease-in-out forwards",
+			'75%': {
+			  opacity: '0%',
 			},
-    },
-  },
-  plugins: [],
-}
-export default config
+			'100%': {
+			  opacity: '100%',
+			},
+		  },
+		  'fade-left': {
+			'0%': {
+			  transform: 'translateX(100%)',
+			  opacity: '0%',
+			},
+			'30%': {
+			  transform: 'translateX(0%)',
+			  opacity: '100%',
+			},
+			'100%': {
+			  opacity: '0%',
+			},
+		  },
+		  'fade-right': {
+			'0%': {
+			  transform: 'translateX(-100%)',
+			  opacity: '0%',
+			},
+			'30%': {
+			  transform: 'translateX(0%)',
+			  opacity: '100%',
+			},
+			'100%': {
+			  opacity: '0%',
+			},
+		  },
+		  'logo': {
+			'0%': {
+			  opacity: '0%',
+			},
+			'75%': {
+			  opacity: '0%',
+			},
+			'100%': {
+			  opacity: '100%',
+			},
+		  },
+		},
+	  },
+	},
+	plugins: [],
+  };
